c6e23c38c6a16d5eaffa1bdb79a492e6915046b5
zsau <qjhjdm@gmail.com>
2017-01-23 08:19:06 -0800

    update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject zsau/feedparser-clj "0.4.2"
-  :description "Parse RSS/Atom feeds with a simple, clojure-friendly API. Forked from scsibug/feedparser-clj."
+  :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive "1.1.6"]
                  [net.java.dev.rome/rome "1.7.1"]]
