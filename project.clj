7149eb8335e6b69977e81478e91752b534d1c4ec
zsau <qjhjdm@gmail.com>
2017-01-23 08:44:15 -0800

    update to new ROME artifact

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [enlive "1.1.6"]
-                 [net.java.dev.rome/rome "1.7.1"]])
+                 [com.rometools/rome "1.7.1"]])
