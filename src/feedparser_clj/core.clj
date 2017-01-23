bddcefc375b1097f57023337f0b1277fa7c9b908
zsau <qjhjdm@gmail.com>
2017-01-23 08:28:29 -0800

    change structs to records

--- src/feedparser_clj/core.clj
+++ src/feedparser_clj/core.clj
@@ -6,26 +6,26 @@
   (:require [net.cgrand.enlive-html :as html])
   (:gen-class))
 
-(defstruct feed :authors :categories :contributors :copyright :description
-           :encoding :entries :feed-type :image :language :link :entry-links
-           :published-date :title :uri)
-(defstruct entry :authors :categories :content :contributors :description
-           :enclosures :link :published-date :title :updated-date :url)
-(defstruct enclosure :length :type :uri)
-(defstruct person :email :name :uri)
-(defstruct category :name :taxonomyURI)
-(defstruct content :type :value)
-(defstruct image :description :link :title :url)
-(defstruct link :href :hreflang :length :rel :title :type)
+(defrecord feed [authors categories contributors copyright description
+                 encoding entries feed-type image language link entry-links
+                 published-date title uri])
+(defrecord entry [authors categories content contributors description
+                  enclosures link published-date title updated-date url])
+(defrecord enclosure [length type uri])
+(defrecord person [email name uri])
+(defrecord category [name taxonomyURI])
+(defrecord content [type value])
+(defrecord image [description link title url])
+(defrecord link [href hreflang length rel title type])
 
-(defn make-enclosure "Create enclosure struct from SyndEnclosure"
+(defn make-enclosure "Create enclosure record from SyndEnclosure"
   [e]
-  (struct-map enclosure :length (.getLength e) :type (.getType e)
-              :url (.getUrl e)))
+  (map->enclosure {:length (.getLength e) :type (.getType e)
+                   :url (.getUrl e)}))
 
-(defn make-content "Create content struct from SyndContent"
+(defn make-content "Create content record from SyndContent"
   [c]
-  (struct-map content :type (.getType c) :value (.getValue c)))
+  (map->content {:type (.getType c) :value (.getValue c)}))
 
 (defn text-content "Get text content from SyndContent"
   [c]
@@ -33,48 +33,48 @@
     (if (not= "html" type) value
       (apply str (html/select (html/html-snippet value) [html/text-node])))))
 
-(defn make-link "Create link struct from SyndLink"
+(defn make-link "Create link record from SyndLink"
   [l]
-  (struct-map link :href (.getHref l) :hreflang (.getHreflang l)
+  (map->link {:href (.getHref l) :hreflang (.getHreflang l)
               :length (.getLength l) :rel (.getRel l) :title (.getTitle l)
-              :type (.getType l)))
+              :type (.getType l)}))
 
-(defn make-category "Create category struct from SyndCategory"
+(defn make-category "Create category record from SyndCategory"
   [c]
-  (struct-map category :name (.getName c)
-              :taxonomyURI (.getTaxonomyUri c)))
+  (map->category {:name (.getName c)
+                  :taxonomyURI (.getTaxonomyUri c)}))
 
-(defn make-person "Create a person struct from SyndPerson"
+(defn make-person "Create person record from SyndPerson"
   [sp]
-  (struct-map person :email (.getEmail sp)
-              :name (.getName sp)
-              :uri (.getUri sp)))
+  (map->person {:email (.getEmail sp)
+                :name (.getName sp)
+                :uri (.getUri sp)}))
 
-(defn make-image "Create image struct from SyndImage"
+(defn make-image "Create image record from SyndImage"
   [i]
-  (struct-map image :description (.getDescription i)
-              :link (.getLink i)
-              :title (.getTitle i)
-              :url (.getUrl i)))
+  (map->image {:description (.getDescription i)
+               :link (.getLink i)
+               :title (.getTitle i)
+               :url (.getUrl i)}))
 
-(defn make-entry "Create feed entry struct from SyndEntry"
+(defn make-entry "Create feed entry record from SyndEntry"
   [e]
-  (struct-map entry :authors (map make-person (seq (.getAuthors e)))
-              :categories (map make-category (seq (.getCategories e)))
-              :content (when-let [c (first (.getContents e))]
-                         (make-content c))
-              :contributors (map make-person (seq (.getContributors e)))
-              :description (if-let [d (.getDescription e)] (make-content d))
-              :enclosures (map make-enclosure (seq (.getEnclosures e)))
-              :link (.getLink e)
-              :published-date (.getPublishedDate e)
-              :title (text-content (.getTitleEx e))
-              :updated-date (.getUpdatedDate e)
-              :uri (.getUri e)))
+  (map->entry {:authors (map make-person (seq (.getAuthors e)))
+               :categories (map make-category (seq (.getCategories e)))
+               :content (when-let [c (first (.getContents e))]
+                          (make-content c))
+               :contributors (map make-person (seq (.getContributors e)))
+               :description (if-let [d (.getDescription e)] (make-content d))
+               :enclosures (map make-enclosure (seq (.getEnclosures e)))
+               :link (.getLink e)
+               :published-date (.getPublishedDate e)
+               :title (text-content (.getTitleEx e))
+               :updated-date (.getUpdatedDate e)
+               :uri (.getUri e)}))
 
-(defn make-feed "Create a feed struct from a SyndFeed"
+(defn make-feed "Create a feed record from a SyndFeed"
   [f]
-  (struct-map feed :authors (map make-person (seq (.getAuthors f)))
+  (map->feed {:authors (map make-person (seq (.getAuthors f)))
               :categories (map make-category (seq (.getCategories f)))
               :contributors (map make-person (seq (.getContributors f)))
               :copyright (.getCopyright f)
@@ -88,7 +88,7 @@
               :entry-links (map make-link (seq (.getLinks f)))
               :published-date (.getPublishedDate f)
               :title (text-content (.getTitleEx f))
-              :uri (.getUri f)))
+              :uri (.getUri f)}))
 
 (defn- parse-internal [xmlreader]
   (let [feedinput (new SyndFeedInput)
