02b84ec1fc7127e2bf6d36a7c015a0ab4dcba875
zsau <qjhjdm@gmail.com>
2018-02-09 11:34:15 -0800

    bump Clojure & ROME deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject zsau/feedparser-clj "0.5.0"
+(defproject zsau/feedparser-clj "0.5.1"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [enlive "1.1.6"]
-                 [com.rometools/rome "1.7.1"]])
+                 [com.rometools/rome "1.9.0"]])
