fb4a219f4d16637944759437726670d19dfc3a1d
zsau <qjhjdm@gmail.com>
2018-02-09 15:41:04 -0800

    bump version to 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zsau/feedparser-clj "0.5.1"
+(defproject zsau/feedparser-clj "0.6.0"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [enlive "1.1.6"]
