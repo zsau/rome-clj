1090f39f06e7199c4e9a62fc6a9cbdbd2a9a48a1
zsau <zsau@fastmail.com>
2018-07-08 19:36:44 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject zsau/feedparser-clj "0.6.0"
+(defproject zsau/feedparser-clj "0.6.1"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [enlive "1.1.6"]
                  [org.apache.httpcomponents/httpclient "4.5.5"]
-                 [com.rometools/rome "1.9.0"]])
+                 [com.rometools/rome "1.11.0"]])
