e6a5d677acbd1e9c5ab8a33803aa363f5b095cbf
zsau <zsau@fastmail.com>
2020-07-21 14:47:37 -0700

    use maps instead of records
    Records are an unnecessary optimization here.

--- src/rome_clj.clj
+++ src/rome_clj.clj
@@ -7,18 +7,6 @@
             [net.cgrand.enlive-html :as html])
   (:gen-class))
 
-(defrecord feed [authors categories contributors copyright description
-                 encoding entries feed-type image language link entry-links
-                 published-date title uri])
-(defrecord entry [authors categories content contributors description
-                  enclosures link published-date title updated-date url])
-(defrecord enclosure [length type uri])
-(defrecord person [email name uri])
-(defrecord category [name taxonomy-uri])
-(defrecord content [type value])
-(defrecord image [description link title url])
-(defrecord link [href hreflang length rel title type])
-
 (defprotocol ToClj
   (->clj [this]))
 
@@ -45,66 +33,66 @@
 (extend-protocol ToClj
   SyndCategory
     (->clj [c]
-      (map->category {:name (.getName c)
-                      :taxonomy-uri (.getTaxonomyUri c)}))
+      {:name (.getName c)
+       :taxonomy-uri (.getTaxonomyUri c)})
   SyndContent
     (->clj [c]
-      (map->content {:type (.getType c)
-                     :value (.getValue c)}))
+      {:type (.getType c)
+       :value (.getValue c)})
   SyndEnclosure
     (->clj [e]
-      (map->enclosure {:length (.getLength e)
-                       :type (.getType e)
-                       :url (.getUrl e)}))
+      {:length (.getLength e)
+       :type (.getType e)
+       :url (.getUrl e)})
   SyndEntry
     (->clj [e]
-      (map->entry {:authors (entry-authors e)
-                   :categories (mapv ->clj (.getCategories e))
-                   :content (some-> e .getContents first ->clj)
-                   :contributors (mapv ->clj (.getContributors e))
-                   :description (some-> e .getDescription ->clj)
-                   :enclosures (mapv ->clj (.getEnclosures e))
-                   :link (.getLink e)
-                   :published-date (.getPublishedDate e)
-                   :title (text-content (.getTitleEx e))
-                   :updated-date (.getUpdatedDate e)
-                   :uri (.getUri e)}))
+      {:authors (entry-authors e)
+       :categories (mapv ->clj (.getCategories e))
+       :content (some-> e .getContents first ->clj)
+       :contributors (mapv ->clj (.getContributors e))
+       :description (some-> e .getDescription ->clj)
+       :enclosures (mapv ->clj (.getEnclosures e))
+       :link (.getLink e)
+       :published-date (.getPublishedDate e)
+       :title (text-content (.getTitleEx e))
+       :updated-date (.getUpdatedDate e)
+       :uri (.getUri e)})
   SyndFeed
     (->clj [f]
-      (map->feed {:authors (mapv ->clj (.getAuthors f))
-                  :categories (mapv ->clj (.getCategories f))
-                  :contributors (mapv ->clj (.getContributors f))
-                  :copyright (.getCopyright f)
-                  :description (.getDescription f)
-                  :encoding (.getEncoding f)
-                  :entries (mapv ->clj (.getEntries f))
-                  :entry-links (mapv ->clj (.getLinks f))
-                  :feed-type (.getFeedType f)
-                  :image (some-> f .getImage ->clj)
-                  :language (.getLanguage f)
-                  :link (.getLink f)
-                  :published-date (.getPublishedDate f)
-                  :title (text-content (.getTitleEx f))
-                  :uri (.getUri f)}))
+      {:authors (mapv ->clj (.getAuthors f))
+       :categories (mapv ->clj (.getCategories f))
+       :contributors (mapv ->clj (.getContributors f))
+       :copyright (.getCopyright f)
+       :description (.getDescription f)
+       :encoding (.getEncoding f)
+       :entries (mapv ->clj (.getEntries f))
+       :entry-links (mapv ->clj (.getLinks f))
+       :feed-type (.getFeedType f)
+       :image (some-> f .getImage ->clj)
+       :language (.getLanguage f)
+       :link (.getLink f)
+       :published-date (.getPublishedDate f)
+       :title (text-content (.getTitleEx f))
+       :uri (.getUri f)})
   SyndImage
     (->clj [i]
-      (map->image {:description (.getDescription i)
-                   :link (.getLink i)
-                   :title (.getTitle i)
-                   :url (.getUrl i)}))
+      {:description (.getDescription i)
+       :link (.getLink i)
+       :title (.getTitle i)
+       :url (.getUrl i)})
   SyndLink
     (->clj [l]
-      (map->link {:href (.getHref l)
-                  :hreflang (.getHreflang l)
-                  :length (.getLength l)
-                  :rel (.getRel l)
-                  :title (.getTitle l)
-                  :type (.getType l)}))
+      {:href (.getHref l)
+       :hreflang (.getHreflang l)
+       :length (.getLength l)
+       :rel (.getRel l)
+       :title (.getTitle l)
+       :type (.getType l)})
   SyndPerson
     (->clj [p]
-      (map->person {:email (.getEmail p)
-                    :name (.getName p)
-                    :uri (.getUri p)})))
+      {:email (.getEmail p)
+       :name (.getName p)
+       :uri (.getUri p)}))
 
 (defn parse [^InputStream istream]
   (-> (SyndFeedInput.)
